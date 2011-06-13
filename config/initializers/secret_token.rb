ce4d25a005c0daea22f9cb6936fad8936dae0606
Zhou Tong <ryan@xwaylab.com>
2011-06-13 20:58:08 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imeridian::Application.config.secret_token = 'a4d599e0625ff4ed4e6f62e33bf3cf5774ee0961b4bfb8a7bc6d391b95d2b5a7babf5f6bd0a64dc8aeb285e82e873241e5353401e80e801db6aa88bb75ed6725'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Imeridian::Application.config.session_store :cookie_store, :key => '_imeridian_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Imeridian::Application.config.session_store :active_record_store
